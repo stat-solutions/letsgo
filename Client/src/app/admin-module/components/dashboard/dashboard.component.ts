3ad66f844ef96e9100cdc3f4ba77b326956d8d6c
gyedasys <gyedasys@gmail.com>
2021-02-19 18:50:33 +0300

    restyling of dashboard data card

--- Client/src/app/admin-module/components/dashboard/dashboard.component.ts
+++ Client/src/app/admin-module/components/dashboard/dashboard.component.ts
@@ -15,127 +15,110 @@ export class DashboardComponent implements OnInit {
   public modalRef: BsModalRef;
   loanTable = [];
   filteredLoans = [];
-  specificLoanTable = []
-  search_customer:string;
-  totalItems:number;
-  id:string;
-  currentPage:number = 1;
+  specificLoanTable = [];
+  searchCustomer: string;
+  totalItems: number;
+  id: string;
+  currentPage = 1;
   pageSize = 10;
   age: number;
-  key:any = "Id"
+  key = 'Id';
+  imageUrl: string;
   csvTable = [];
-   @ViewChild('exportTable')exportExcel:ElementRef
-//excel sheet name
-fileName = "loanInfo.xlsx";
+  @ViewChild('exportTable') exportExcel: ElementRef;
+  // excel sheet name
+  fileName = 'loanInfo.xlsx';
 
   constructor(
     private modalService: BsModalService,
-    private landingPage: LoaningService,
+    private loaning: LoaningService,
     private spinner: NgxSpinnerService,
     private fb: FormBuilder,
     private router: Router,
-    private activatedRouter:ActivatedRoute
+    private activatedRouter: ActivatedRoute
   ) {}
 
-  ngOnInit() {
-    setTimeout(() => {
-      this.landingPage.getloanData().subscribe(userData => {
-       this.loanTable = userData.map(eachUser => {
-          const oldDate = eachUser.CreatedAt
-          const diffInDates = moment(this.age).diff(moment(oldDate))
-          const timeInMonths = moment(diffInDates).format('MM [months] DD [days]')
-          return { ...eachUser, TotalAge:timeInMonths }
-        })
-           this.filteredLoans = this.loanTable;
-           this.totalItems = this.filteredLoans.length;
-
-
-      })
-    },0)
+  ngOnInit(): void {
+    this.loaning.getAllLoanDetails().subscribe((userData) => {
+      this.loanTable = userData.map((eachUser) => {
+        const oldDate = eachUser.CreatedAt;
+        const diffInDates = moment(this.age).diff(moment(oldDate));
+        const timeInMonths = moment(diffInDates).format(
+          'MM [months] DD [days]'
+        );
+        return { ...eachUser, TotalAge: timeInMonths };
+      });
+      this.filteredLoans = this.loanTable;
+      this.totalItems = this.filteredLoans.length;
+    });
   }
 
-
-  checkTable(array: Array<any>) {
+  checkTable(array: Array<any>): any {
     return array.length ? true : false;
   }
 
-  checkLoanStatus(array: Array<any>, loanStatus: string) {
+  checkLoanStatus(array: Array<any>, loanStatus: string): any {
     return array.filter((userData) => userData.LoanStatus === loanStatus)
       .length;
   }
 
-  getValue(event) {
-    console.log(event.target.value)
-    this.search_customer = event.target.value
-    if(event.target.value === ''){
-      this.filteredLoans = this.loanTable
+  getValue(event): any {
+    console.log(event.target.value);
+    this.searchCustomer = event.target.value;
+    if (event.target.value === '') {
+      this.filteredLoans = this.loanTable;
       this.totalItems = this.filteredLoans.length;
-
-
-    }
-    else{
-          this.filteredLoans =  this.filterCustomer(this.search_customer)
-          this.totalItems = this.filteredLoans.length;
-
+    } else {
+      this.filteredLoans = this.filterCustomer(this.searchCustomer);
+      this.totalItems = this.filteredLoans.length;
     }
   }
-  filterCustomer(searchTerm:string){
-    if(searchTerm)
-
-    return this.filteredLoans.filter(
-      loan=>
-      loan.Customer.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1
-      ||loan.Stage.toLowerCase().indexOf(searchTerm.toLowerCase())!==   -1
-      ||loan.Status.toLowerCase().indexOf(searchTerm.toLowerCase())!==  -1
-      ||loan.LoanProduct.toLowerCase().indexOf(searchTerm.toLowerCase())!==-1
-      ||loan.LoanType.toLowerCase().indexOf(searchTerm.toLowerCase())!==-1
-      )
-
+  filterCustomer(searchTerm: string): any {
+    if (searchTerm) {
+      return this.filteredLoans.filter(
+        (loan) =>
+          loan.Customer.toLowerCase().indexOf(searchTerm.toLowerCase()) !==
+            -1 ||
+          loan.Stage.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1 ||
+          loan.Status.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1 ||
+          loan.LoanProduct.toLowerCase().indexOf(searchTerm.toLowerCase()) !==
+            -1 ||
+          loan.LoanType.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1
+      );
+    }
+  }
+  pageChanged(event): any {
+    this.currentPage = event;
+    console.log(this.filteredLoans.length);
+    // get
+    // if(currentPage )
   }
 
-  clickOnCustomer(id:number){
-        this.router.navigate(['admin/customerdetails', id], )
-  }
-   pageChanged(event){
-     this.currentPage = event
-     console.log(this.filteredLoans.length)
-     //get
-     //if(currentPage )
-
-   }
-
-  public openModal(template: TemplateRef<any>, id:number, index:number) {
-    console.log(id, index)
-  this.landingPage.getLoanDetails(id).subscribe(details=>{
-    console.log(details)
-    this.specificLoanTable.push(details)
-  })
-  console.log(this.specificLoanTable)
-  if(this.checkTable(this.specificLoanTable)){
-       this.modalRef = this.modalService.show(
+  public openModal(template: TemplateRef<any>, photoUrl: string): any {
+    this.imageUrl = photoUrl;
+    this.modalRef = this.modalService.show(
       template,
       Object.assign({}, { class: 'modal-lg modal-dialog-center' })
     );
   }
-  }
-  
-  //export to excel
-  exportToExcel(){
-    const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(this.exportExcel.nativeElement);
 
-    //create a workbook and add work sheet
-    const wb:XLSX.WorkBook = XLSX.utils.book_new()
+  // export to excel
+  exportToExcel(): any {
+    const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(
+      this.exportExcel.nativeElement
+    );
+
+    // create a workbook and add work sheet
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(wb, ws, 'Sheet 1');
 
-    //save fileName
-    XLSX.writeFile(wb, this.fileName)
+    // save fileName
+    XLSX.writeFile(wb, this.fileName);
   }
 
-
-
-   reverse:boolean = false;
-  sort(item:string){
+  reverse: boolean = false;
+  sort(item: string) {
     this.key = item;
-    this.reverse = !this.reverse
+    this.reverse = !this.reverse;
   }
 }
--- Client/src/app/application-module/components/dashboard/dashboard.component.ts
+++ Client/src/app/application-module/components/dashboard/dashboard.component.ts
@@ -39,15 +39,17 @@ fileName = "loanInfo.xlsx";
   ) {}
 
   ngOnInit(): any {
-    this.landingPage.getloanData().subscribe(userData => {
-    this.loanTable = userData.map(eachUser => {
+    this.landingPage.getAllLoanDetails().subscribe((userData) => {
+      this.loanTable = userData.map((eachUser) => {
         const oldDate = eachUser.CreatedAt;
         const diffInDates = moment(this.age).diff(moment(oldDate));
-        const timeInMonths = moment(diffInDates).format('MM [months] DD [days]');
+        const timeInMonths = moment(diffInDates).format(
+          'MM [months] DD [days]'
+        );
         return { ...eachUser, TotalAge: timeInMonths };
       });
-    this.filteredLoans = this.loanTable;
-    this.totalItems = this.filteredLoans.length;
+      this.filteredLoans = this.loanTable;
+      this.totalItems = this.filteredLoans.length;
     });
     this.landingPage.getAllLoanDetails().subscribe(userData => {
       res => console.log(res)
