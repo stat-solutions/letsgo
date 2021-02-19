982f91a23da777c9016447d76102c029540e1322
gyedasys <gyedasys@gmail.com>
2021-02-19 03:29:47 +0300

    styling of loan creation

--- Client/src/app/admin-module/components/approve-users/approve-users.component.ts
+++ Client/src/app/admin-module/components/approve-users/approve-users.component.ts
@@ -4,16 +4,17 @@ import { NgxSpinnerService } from 'ngx-spinner';
 import {BsModalService, BsModalRef} from 'ngx-bootstrap/modal';
 import { Router } from '@angular/router';
 import { UsersService } from 'src/app/shared/services/users.service';
+import * as XLSX from 'xlsx';
 import { AuthServiceService } from 'src/app/shared/services/auth-service.service';
 @Component({
   selector: 'app-approve-users',
   templateUrl: './approve-users.component.html',
   styleUrls: ['./approve-users.component.scss'],
 })
-
 export class ApproveUsersComponent implements OnInit {
   users: any;
   filteredUsers: any;
+  fileName = 'users.xlsx';
   reverse = false;
   approvedUsers: any;
   roles: any;
@@ -25,114 +26,127 @@ export class ApproveUsersComponent implements OnInit {
   makeUserApproved = [];
   disableButton = true;
   key: any = 'userId';
-  @ViewChild('exportTable')element: ElementRef;
+  @ViewChild('exportTable') element: ElementRef;
   // tslint:disable-next-line: variable-name
   search_user: string;
   User = this.authService.loggedInUserInfo();
   // tslint:disable-next-line: no-shadowed-variable
   constructor(
-              private authService: AuthServiceService,
-              private userService: UsersService,
-              private spinner: NgxSpinnerService,
-              private fb: FormBuilder,
-              private bsModalService: BsModalService,
-              private router: Router
-              ) { }
+    private authService: AuthServiceService,
+    private userService: UsersService,
+    private spinner: NgxSpinnerService,
+    private fb: FormBuilder,
+    private bsModalService: BsModalService,
+    private router: Router
+  ) {}
   ngOnInit(): void {
     this.getUserToApproval();
     this.getRoles();
     this.userRole = this.fb.group({
-        role: ['', Validators.required]
+      role: ['', Validators.required],
     });
   }
-  getUserToApproval(): any{
+  getUserToApproval(): any {
     this.userService.getUserForApproval().subscribe(
-      res => {
+      (res) => {
         this.users = res;
         this.filteredUsers = this.users;
         // console.log(this.filteredUsers);
       },
-      err => console.log(err.error.error.message)
+      (err) => console.log(err.error.error.message)
     );
   }
   getRoles(): any {
     this.userService.getUserRoles().subscribe(
-      res => {
+      (res) => {
         this.roles = res;
         // tslint:disable-next-line: only-arrow-functions
-        this.roles = this.roles.map(function(role: any): any {
+        this.roles = this.roles.map(function (role: any): any {
           return {
             roleId: role.roleId,
-            roleName: role.roleName.replace(/_/g, ' ').toUpperCase()
+            roleName: role.roleName.replace(/_/g, ' ').toUpperCase(),
           };
-      });
+        });
         // console.log(this.roles);
       },
-      err => console.log(err.error.error.message)
+      (err) => console.log(err.error.error.message)
     );
   }
-  goToUsers(): any{
+  goToUsers(): any {
     this.router.navigate(['admin/users']);
   }
 
   // closemodal popup
-  closeModal(): any{
+  closeModal(): any {
     this.bsModalRef.hide();
   }
 
   // getrole FormControl
-  get fval(): any{
+  get fval(): any {
     return this.userRole.controls;
   }
   getValue(event): any {
     this.search_user = event.target.value;
-    if (event.target.value === ''){
+    if (event.target.value === '') {
       this.filteredUsers = this.users;
       //  this.totalItems = this.users.length;
-    }
-    else{
-          this.filteredUsers =  this.filterUser(this.search_user);
-          //  this.totalItems = this.users.length;
+    } else {
+      this.filteredUsers = this.filterUser(this.search_user);
+      //  this.totalItems = this.users.length;
     }
   }
 
-  filterUser(searchTerm: string): any{
+  filterUser(searchTerm: string): any {
     if (searchTerm) {
       console.log(searchTerm);
       return this.filteredUsers.filter(
-        user  =>
-        user.userName.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1
-        ||  user.userEmail.toLowerCase().indexOf(searchTerm.toLowerCase())  !==   -1
-        );
-      }
+        (user) =>
+          user.userName.toLowerCase().indexOf(searchTerm.toLowerCase()) !==
+            -1 ||
+          user.userEmail.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1
+      );
+    }
   }
-  sort(item: string): any{
+  sort(item: string): any {
     this.key = item;
     this.reverse = !this.reverse;
   }
-  assignRole(event): any{
+  assignRole(event): any {
     this.fval.role.setValue(event.target.value);
     this.disableButton = false;
-}
+  }
 
-  approveUser(template: TemplateRef<any>, id: number, index: number): any{
-    this.bsModalRef =  this.bsModalService.show(template);
+  exportToExcel(): any {
+    // pass the table to worksheet
+    // const element =  document.getElementById('export-table');
+    const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(this.element);
+
+    // create a workbook and add work sheet
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet 1');
+
+    // save fileName
+    XLSX.writeFile(wb, this.fileName);
+  }
+
+  approveUser(template: TemplateRef<any>, id: number, index: number): any {
+    this.bsModalRef = this.bsModalService.show(template);
     this.makeUserApproved.push(this.users[index]);
     // console.log(this.makeUserApproved);
     this.userId = id;
     this.userIndex = index;
   }
 
-  rejectUser(userInfo: any , id, index): any{
+  rejectUser(userInfo: any, id, index): any {
     // console.log(this.fval.role.value);
     this.disableButton = !this.disableButton;
     const data = {
-       userId: userInfo[0].userId,
-      };
+      userId: userInfo[0].userId,
+    };
     // console.log(data);
     this.userService.rejectUser(data).subscribe(
-      res => {},
-      err => console.log(err.error.error.message)
+      (res) => {},
+      (err) => console.log(err.error.error.message)
     );
     this.fval.role.reset();
     this.closeModal();
@@ -142,31 +156,30 @@ export class ApproveUsersComponent implements OnInit {
     }, 1000);
   }
 
-   approvedUser(userInfo: any , id, index): any{
-      // console.log(this.fval.role.value);
-      this.disableButton = !this.disableButton;
-      const data = {
-         userId: userInfo[0].userId,
-         userStatus: 2,
-         roleId: null,
-         userIdApprover: this.User.userId
-        };
-      this.roles.forEach(role => {
-        if (this.fval.role.value === role.roleName) {
-          data.roleId = role.roleId;
-        }
-      });
-      // console.log(data);
-      this.userService.approveUser(data).subscribe(
-        res => {},
-        err => console.log(err.error.error.message)
-      );
-      this.fval.role.reset();
-      this.closeModal();
-      setTimeout(() => {
-        // this.spinner.hide();
-        location.reload();
-      }, 1000);
-   }
-
+  approvedUser(userInfo: any, id, index): any {
+    // console.log(this.fval.role.value);
+    this.disableButton = !this.disableButton;
+    const data = {
+      userId: userInfo[0].userId,
+      userStatus: 2,
+      roleId: null,
+      userIdApprover: this.User.userId,
+    };
+    this.roles.forEach((role) => {
+      if (this.fval.role.value === role.roleName) {
+        data.roleId = role.roleId;
+      }
+    });
+    // console.log(data);
+    this.userService.approveUser(data).subscribe(
+      (res) => {},
+      (err) => console.log(err.error.error.message)
+    );
+    this.fval.role.reset();
+    this.closeModal();
+    setTimeout(() => {
+      // this.spinner.hide();
+      location.reload();
+    }, 1000);
+  }
 }
